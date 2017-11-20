767188a9d663fdb71208f591fd8706198dc545b3
Heather Marie Haas <osxgirl@mac.com>
2017-11-20 22:41:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,17 +108,14 @@ def current_player(board)
   end
 end
 
+
 def turn(board)
-  puts "Please enter 1-9:"
-    userInput = gets.strip
-    index = input_to_index(userInput)
-    if !valid_move?(board, index)
-      turn(board)
-    else
-      move(board, index, current_player(board))
-    end
-  display_board(board)
-end
+    puts "Please enter 1-9:"
+    position = gets.chomp
+    move(board,position)
+    display_board(board)
+ end
+
 
 def play(board)
   while !over?(board) && !won?(board) && !draw?(board)
